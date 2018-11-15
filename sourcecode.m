29030223d63d702941fff9f7f3a4d50abc37897e
SamiHagrai <45062174+SamiHagrai@users.noreply.github.com>
2018-11-15 09:42:41 +0100

    Update sourcecode.m

--- sourcecode.m
+++ sourcecode.m
@@ -5,7 +5,7 @@
 % 
 % http://matlab-recognition-code.com/signature-recognition-based-on-neural-networks/
 %
-% For more informations please email me Sami_Hagrai-88@@matlab-recognition-code.com
+% For more informations please email me Sami_Hagrai-88@matlab-recognition-code.com
 
 %
 %
